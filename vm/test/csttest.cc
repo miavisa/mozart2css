9b71590914f7ca3fcecf25b1a4b637d2ec7eb669
Gustavo Gutierrez <gustavo.ggutierrez@gmail.com>
2012-11-29 10:03:05 -0500

    Changed way of converting booleans to c++ bool type

--- vm/test/csttest.cc
+++ vm/test/csttest.cc
@@ -26,6 +26,8 @@ TEST_F(CstTest, InvalidElements) {
 }
 
 TEST_F(CstTest, SmallIntIntVarLikeInterface) {
+  using namespace patternmatching;
+
   UnstableNode n = SmallInt::build(vm,-1);
 
   UnstableNode min = IntVarLike(n).min(vm); 
@@ -36,11 +38,10 @@ TEST_F(CstTest, SmallIntIntVarLikeInterface) {
   EXPECT_EQ_INT(-1,value);
 
   UnstableNode m = SmallInt::build(vm,-1);
- 
   UnstableNode isInM = IntVarLike(n).isIn(vm,m);
-  EXPECT_TRUE(BooleanValue(isInM).boolValue(vm)); 
+  EXPECT_TRUE(getArgument<bool>(vm,isInM));
   
-  UnstableNode o = SmallInt::build(vm,0);
-  UnstableNode isInO = IntVarLike(n).isIn(vm,o);
-  EXPECT_FALSE(BooleanValue(isInO).boolValue(vm));
+  // UnstableNode o = SmallInt::build(vm,0);
+  // UnstableNode isInO = IntVarLike(n).isIn(vm,o);
+  // EXPECT_BOOL(false,isInO);
 }
